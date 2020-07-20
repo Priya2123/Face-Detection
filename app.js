be07f1764010c73b294cb0effc02ccd432555837
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-20 17:04:59 +0530

    added models

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const video = document.getElementById("video");
 
+//runs all async calls
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"), //register different parts of your face
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"), //box around face
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
+
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -7,5 +15,14 @@ function startVideo() {
     (err) => console.error(err)
   );
 }
+
+video.addEventListener("play", () => {
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+  }, 100);
+});
 
 startVideo();
